fb90b7f6093f18b8d397159ec019ae93439a4a6d
unknown <dimarconicolas5@gmail.com>
2026-03-15 18:01:43 +0100

    fix vercel routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
 
 import { routes } from './app.routes';
 
@@ -9,16 +13,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     // 1. Configuramos el router con scroll automático y transiciones suaves
     provideRouter(
-      routes, 
+      routes,
       withComponentInputBinding(), // Permite recibir params de ruta como inputs (Muy moderno)
-      withViewTransitions(),       // Transiciones de vista nativas del navegador
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }) // Scroll arriba al navegar
+      withViewTransitions(), // Transiciones de vista nativas del navegador
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }), // Scroll arriba al navegar
     ),
-    
+
     // 2. Cliente HTTP para las APIs
     provideHttpClient(),
-    
-
-   
-  ]
+  ],
 };
